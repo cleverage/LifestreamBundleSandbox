2d0f0fbaa23bd71c0a0a82b8768ff22c096d99dd
Xavier Lacot <xavier@lacot.org>
2011-03-24 21:49:25 +0100

    added the bundle in the kernel :)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Acme\DemoBundle\AcmeDemoBundle(),
-//            new CleverAge\Bundle\LifestreamBundle\LifestreamBundle(),
+            new CleverAge\Bundle\LifestreamBundle\CleverAgeLifestreamBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
